b0507aec36affaec54378d7d46c152cf56fc99d2
Euan Macfarlane <euanmacfarlane@MacBook-Pro.lan>
2022-07-04 15:10:51 +0100

    test written for getReviewByID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,7 @@ const {
   reviewData,
   userData,
 } = require("../db/data/test-data/index");
+const { get } = require("express/lib/response");
 
 beforeEach(() => seed({ categoryData, commentData, reviewData, userData }));
 afterAll(() => db.end());
@@ -25,6 +26,29 @@ describe("app tests", () => {
             categories.forEach((category) => {
               expect(category).toHaveProperty("slug");
               expect(category).toHaveProperty("description");
+            });
+          });
+      });
+    });
+    describe("GET /api/reviews/:review_id", () => {
+      test("200: responds with review object passed by user", () => {
+        const REVIEW_ID = 1;
+        return request(app)
+          .get(`/api/reviews/${REVIEW_ID}`)
+          .expect(200)
+          .then(({ body: { reviews } }) => {
+            const reviewKeys = Object.keys(reviews[0]);
+            expect(reviewKeys).toHaveLength(9);
+            reviews.forEach((review) => {
+              expect(review).toHaveProperty("review_id");
+              expect(review).toHaveProperty("title");
+              expect(review).toHaveProperty("review_body");
+              expect(review).toHaveProperty("designer");
+              expect(review).toHaveProperty("review_img_url");
+              expect(review).toHaveProperty("votes");
+              expect(review).toHaveProperty("category");
+              expect(review).toHaveProperty("owner");
+              expect(review).toHaveProperty("created_at");
             });
           });
       });
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+app.get("/api/reviews/:/review_id", getReviewByID);
+
 app.use("*", (req, res) => {
   res.status(404).send({ msg: "Invalid Path" });
 });
