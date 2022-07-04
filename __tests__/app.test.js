a3db4d95a8f9dd16ba8f6676ec3352130dd6cb39
Euan Macfarlane <euanmacfarlane@macbook-pro.lan>
2022-07-04 12:49:54 +0100

    added models.js, controllers.js, updated files to run 1st test GET categories

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,3 +11,24 @@ const {
 
 beforeEach(() => seed({ categoryData, commentData, reviewData, userData }));
 afterAll(() => db.end());
+
+describe("app tests", () => {
+  describe("app Hppy Path", () => {
+    describe("GET /api/categories", () => {
+      test("200: responds with a body of categories", () => {
+        return request(app)
+          .get("/api/categories")
+          .expect(200)
+          .then(({ body: { categories } }) => {
+            console.log(categories);
+            const categoryKeys = Object.keys(categories[0]);
+            expect(categoryKeys).toHaveLength(2);
+            categories.forEach((category) => {
+              expect(category).toHaveProperty("slug");
+              expect(category).toHaveProperty("description");
+            });
+          });
+      });
+    });
+  });
+});
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
-
 const app = express();
+const { getCategories } = require("./db/controllers/controllers");
+
+app.use(express.json());
+
+app.get("/api/categories", getCategories);
 
 module.exports = app;
