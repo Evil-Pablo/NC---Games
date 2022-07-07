2d29d899fb146d7553bba95abeaf504d19ab09df
Euan Macfarlane <euanmacfarlane@macbook-pro.lan>
2022-07-07 11:15:56 +0100

    update test file for reviews-comment-count update models query to pull comment count as an integer

--- db/models/models.js
+++ db/models/models.js
@@ -9,7 +9,7 @@ exports.selectCategories = () => {
 exports.selectReviewByID = (reviewID) => {
   return db
     .query(
-      "SELECT reviews.*, COUNT(*) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;",
+      "SELECT reviews.*, CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id;",
       [reviewID]
     )
     .then((result) => {
--- db/test.sql
+++ db/test.sql
@@ -1,6 +1,6 @@
 \c nc_games_test
 
-    SELECT reviews.*, COUNT(*) AS comment_count FROM reviews
+    SELECT reviews.*, CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
-    WHERE reviews.review_id = 2
+    WHERE reviews.review_id = 1
     GROUP BY reviews.review_id;
