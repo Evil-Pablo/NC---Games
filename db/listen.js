d65e398c1f0e71b2d3fb19ed59f5c114700ced23
Euan Macfarlane <euanmacfarlane@MacBook-Pro.lan>
2022-07-29 09:38:30 +0100

    updates listen.js

--- db/listen.js
+++ db/listen.js
@@ -1,7 +1,7 @@
 const app = require("../app");
 const { PORT = 9090 } = process.env;
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) throw err;
   console.log(`Listening on ${PORT}...`);
 });
